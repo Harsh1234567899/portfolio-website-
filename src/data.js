0b401472a2f6307c4475574e651acf7f94eead9d
Harsh1234567899 <pankhaniyaharsh222@gmail.com>
2025-12-30 16:47:14 +0530

    add new projects

--- src/data.js
+++ src/data.js
@@ -10,15 +10,27 @@ export const personalInfo = {
       image: "/portfolio.png", 
     },
     {
-      title: "Calculator",
-      desc: "A Calculator with <strong>HTML & CSS & javascript",
-      image: "/calculator.png",
+      title: "Blog type Forntend",
+      desc: "An a blog website for post and read other blog , use Appwrite for backend service.<br>Technology use <strong>React+vite , tinyMCE for text editer , rect hook form inbuild react form</storng>",
+      image: "/blogWebsite.png",
+      link: "https://react-learning-git-main-pankhaniyaharsh222-gmailcoms-projects.vercel.app/all-post",
     },
     {
       title: "Movie Tickit Booking System",
       desc: "A Movie Ticket Booking Sytem for Collage project with admin panel with <strong>React.js & Node.js & javascript",
       image: "/MovieTicketBooking.png",
       link: "https://movie-ticket-booking-gamma-six.vercel.app/",
+    },
+    {
+      title: "Backend for youtube Clone ",
+      desc: "Visit my github repo and check out code <b>click on this card</b>. Create a backend for Youtube clone website using <strong>Node.js & Express.js & mongoose </strong> with authentication. api like login , signup , admin login , videos and photo upload in cloudinary",
+      image: "/backend_text_1.jpg",
+      link: "https://github.com/Harsh1234567899/backend-with-javaScript"
+    },
+    {
+      title: "Calculator",
+      desc: "A Calculator with <strong>HTML & CSS & javascript",
+      image: "/calculator.png",
     },
     {
       title: "Static Website",
